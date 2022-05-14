192d051535b5c2ce6d4bb8db24a9b32a35504610
angelique-df <faye.angelique.d@gmail.com>
2022-05-14 16:36:38 +0000

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", () => {
+	/**
+	 * The parent element for the component
+	 */
+	const sectionElement = document.querySelector("section");
+	/**
+	 * The radio inputs
+	 */
+	const inputElements = document.querySelectorAll('input[type="radio"]');
+	/**
+	 * The submit input
+	 */
+	const submitElement = document.querySelector('input[type="submit"]');
+
+});
