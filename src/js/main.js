e20d455acce4af1fe44b7d52facc51a4bc3bcd2d
angelique-df <faye.angelique.d@gmail.com>
2022-05-14 16:40:03 +0000

    Update main.js
    Add `RatingView`

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
+/**
+ * Displays the views
+ */
+class RatingView {
+	displayThankYouState(sectionElement, ratingSelected) {
+		const thankyouStateTemplate = `
+			<img
+				id="thank-you-illustration"
+				src="src/images/illustration-thank-you.svg"
+				alt=""
+			/>
+			<!-- Add rating here -->
+			<p class="selection-recap">
+				You selected <span id="rating-selected">${ratingSelected}</span> out of 5
+			</p>
+			<p class="copy-heading">Thank you!</p>
+			<p>
+				We appreciate you taking the time to give a rating. If you ever need
+				more support, don’t hesitate to get in touch!
+			</p>
+			`;
+
+		// Add a class so that the styles work
+		sectionElement.classList.add("thank-you-state");
+		// Replace the element's HTML content
+		sectionElement.innerHTML = thankyouStateTemplate;
+		console.log("Reload the page to select another rating!");
+	}
+}
+
 window.addEventListener("DOMContentLoaded", () => {
 	/**
 	 * The parent element for the component
